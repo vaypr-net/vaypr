80b9cd9f776c0a5079f05edbf84efa3eb39de01a
Saad Dev <saaddev@Saads-MacBook-Pro.local>
2026-02-12 11:52:51 +0500

    fix build issue

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `Vayper v1.0.2 - Invoice Software | Deployed: ${new Date().toISOString().split('T')[0]}`;
   }
 }
